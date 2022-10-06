512b62fc0691c623e08472842188749d5f23e0e7
Sunny619 <abhigyan260902@gmail.com>
2022-10-06 18:47:01 +0530

    Initial Commit

--- public/script.js
+++ public/script.js
@@ -1,7 +1,12 @@
 const socket = io('/')
 const videoGrid = document.getElementById('video-grid')
+let templates = document.querySelector('#template')
+let viewportHeight = window.innerHeight;
+let viewportWidth = window.innerWidth
+let count = 1;
 const myPeer = new Peer({host:'peerjs-server.herokuapp.com', secure:true, port:443})
 const myVideo = document.createElement('video')
+let flag = 0;
 myVideo.muted = true
 const peers = {}
 navigator.mediaDevices.getUserMedia({
@@ -14,7 +19,10 @@ navigator.mediaDevices.getUserMedia({
     call.answer(stream)
     const video = document.createElement('video')
     call.on('stream', userVideoStream => {
-      addVideoStream(video, userVideoStream)
+      console.log("Peercall called");
+      flag^=1;
+      if(flag)
+        addVideoStream(video, userVideoStream)
     })
   })
 
@@ -24,6 +32,7 @@ navigator.mediaDevices.getUserMedia({
 })
 
 socket.on('user-disconnected', userId => {
+  count--;
   if (peers[userId]) peers[userId].close()
 })
 
@@ -35,10 +44,14 @@ function connectToNewUser(userId, stream) {
   const call = myPeer.call(userId, stream)
   const video = document.createElement('video')
   call.on('stream', userVideoStream => {
-    addVideoStream(video, userVideoStream)
+    console.log("New user stream called");
+    flag^=1;
+    if(flag)
+      addVideoStream(video, userVideoStream)
   })
   call.on('close', () => {
-    video.remove()
+    //video.remove()
+    video.parentElement.parentElement.parentElement.remove()
   })
 
   peers[userId] = call
@@ -47,7 +60,37 @@ function connectToNewUser(userId, stream) {
 function addVideoStream(video, stream) {
   video.srcObject = stream
   video.addEventListener('loadedmetadata', () => {
+    
     video.play()
   })
-  videoGrid.append(video)
+  blitNewUserVideo(video);
+  //videoGrid.append(video)
 }
+
+function blitNewUserVideo(video)
+{
+  count++;
+  let card = templates.content.cloneNode(true).querySelector('#call-card');
+  console.log(card);
+  let videoHolder = card.children[0].children[0];
+  videoHolder.append(video);
+  videoGrid.appendChild(card);
+}
+// window.addEventListener('resize', resize());
+
+// function resize()
+// {
+//   const videos = document.getElementsByTagName("video");
+//   if(videos.length>)
+//   else if(videos.length>4)
+//   {
+
+//   }
+//   else if(videos.length>2)
+//   {
+
+//   }
+//   else if(videos.length==2)
+
+
+// }
--- server.js
+++ server.js
@@ -1,31 +1,64 @@
 const express = require('express')
+const { get } = require('http')
 const app = express()
 const server = require('http').Server(app)
 const io = require('socket.io')(server)
 const { v4: uuidV4 } = require('uuid')
 
 const port  = process.env.PORT || 3000
-
+let rooms = new Map()
+let users = new Set()
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
+app.use(express.static('views'));
 
 app.get('/', (req, res) => {
   res.redirect(`/${uuidV4()}`)
 })
 
+app.get('/users', (req, res) => {
+  res.send("Users: "+ Array.from(users))
+})
+app.get('/rooms', (req, res) => {
+  reply = "<br>"
+  rooms.forEach((value, key) => {
+    reply+="<br>"
+    reply +="<b>"+ key +" :</b> "
+    reply += Array.from(value)
+  });
+  res.send("Rooms: "+  reply)
+  //console.log(rooms)
+})
 app.get('/:room', (req, res) => {
   res.render('room', { roomId: req.params.room })
 })
 
 io.on('connection', socket => {
   socket.on('join-room', (roomId, userId) => {
+    MapUser(roomId, userId)
     socket.join(roomId)
     socket.to(roomId).emit('user-connected', userId)
 
     socket.on('disconnect', () => {
       socket.to(roomId).emit('user-disconnected', userId)
+      users.delete(userId);
     })
   })
 })
 
+function MapUser(roomId, userId)
+{
+  if(!rooms.has(roomId))
+  {
+    let room = new Set();
+    room.add(userId);
+    rooms.set(roomId,room);
+  }
+  else
+  {
+    let room = rooms.get(roomId)
+    room.add(userId)
+  }
+  users.add(userId);
+}
 server.listen(port)

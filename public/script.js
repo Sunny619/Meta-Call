df27c7bdf4d6991b8a250bf96b18077a0f746823
Sunny619 <abhigyan260902@gmail.com>
2023-03-27 14:02:08 +0530

    Tried adding face mesh to call

--- public/script.js
+++ public/script.js
@@ -15,6 +15,53 @@ class User{
     this.video = video
   }
 }
+const videoElement = document.getElementsByClassName('input_video')[0];
+const canvasElement = document.getElementsByClassName('output_canvas')[0];
+const canvasCtx = canvasElement.getContext('2d');
+
+function onResults(results) {
+    canvasCtx.save();
+    canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height);
+    canvasCtx.drawImage(
+        results.image, 0, 0, canvasElement.width, canvasElement.height);
+    if (results.multiFaceLandmarks) {
+        for (const landmarks of results.multiFaceLandmarks) {
+            drawConnectors(canvasCtx, landmarks, FACEMESH_TESSELATION,
+                { color: '#C0C0C070', lineWidth: 1 });
+            drawConnectors(canvasCtx, landmarks, FACEMESH_RIGHT_EYE, { color: '#FF3030' });
+            drawConnectors(canvasCtx, landmarks, FACEMESH_RIGHT_EYEBROW, { color: '#FF3030' });
+            drawConnectors(canvasCtx, landmarks, FACEMESH_RIGHT_IRIS, { color: '#FF3030' });
+            drawConnectors(canvasCtx, landmarks, FACEMESH_LEFT_EYE, { color: '#30FF30' });
+            drawConnectors(canvasCtx, landmarks, FACEMESH_LEFT_EYEBROW, { color: '#30FF30' });
+            drawConnectors(canvasCtx, landmarks, FACEMESH_LEFT_IRIS, { color: '#30FF30' });
+            drawConnectors(canvasCtx, landmarks, FACEMESH_FACE_OVAL, { color: '#E0E0E0' });
+            drawConnectors(canvasCtx, landmarks, FACEMESH_LIPS, { color: '#E0E0E0' });
+        }
+    }
+    canvasCtx.restore();
+}
+
+const faceMesh = new FaceMesh({
+    locateFile: (file) => {
+        return `https://cdn.jsdelivr.net/npm/@mediapipe/face_mesh/${file}`;
+    }
+});
+faceMesh.setOptions({
+    maxNumFaces: 1,
+    refineLandmarks: true,
+    minDetectionConfidence: 0.5,
+    minTrackingConfidence: 0.5
+});
+faceMesh.onResults(onResults);
+
+const camera = new Camera(videoElement, {
+    onFrame: async () => {
+        await faceMesh.send({ image: videoElement });
+    },
+    width: 1280,
+    height: 720
+});
+camera.start();
 const users = new Map();
 var viewportHeight = window.innerHeight;
 let viewportWidth = window.innerWidth;
@@ -34,20 +81,20 @@ navigator.mediaDevices.getUserMedia({
   audio: true
 }).then(stream => {
   myStream = stream;
-  addVideoStream(myVideo, stream)
+  addVideoStream(myVideo, canvasElement.captureStream())
 
   myPeer.on('connection', conn => {
     handleConnection(conn)
   });
 
   myPeer.on('call', call => {
-    call.answer(stream)
+    call.answer(canvasElement.captureStream())
     handleCall(call);
     console.log("Oncall");
   });
 
   socket.on('user-connected', userId => {
-    connectToNewUser(userId, stream)
+    connectToNewUser(userId, canvasElement.captureStream())
     console.log("User connected");
   })
 })

e744960be6a074583d111794d6cab2d1b639a3b3
Sunny619 <abhigyan260902@gmail.com>
2023-04-01 10:44:42 +0530

    Fixed asnyc update issues

--- public/script.js
+++ public/script.js
@@ -7,9 +7,7 @@ const videoButton = document.getElementById('cam-button')
 const muteButton = document.getElementById('mute-button')
 const endButton = document.getElementById('end-button')
 var chat = document.getElementById("message-container"); 
-document.getElementById("inputName").value = NAME
-document.getElementById("inputPass").value = PASS
-document.getElementById("inputRoom").value = ROOM_ID
+
 //Class Declarations
 class User{
   constructor(id, name,video){
@@ -154,6 +152,12 @@ function blitNewUserVideo(video,userId) {
   let videoHolder = card.children[0].children[0];
   videoHolder.append(video);
   videoGrid.appendChild(card);
+  if(ROOM_ID!= undefined)
+  {
+    document.getElementById("inputName").value = NAME
+    document.getElementById("inputPass").value = PASS
+    document.getElementById("inputRoom").value = ROOM_ID
+  }
   fetch("/name?uid="+ userId)
     .then((response) => response.json())
     .then((json) => updateNameArgs(json.name,userId));
